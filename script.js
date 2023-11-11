e7ab4f3edc8434b10c85c9ba9ff7224aa1415f62
Caiari Tolotti <caiari.tolotti@gmail.com>
2023-11-11 12:26:02 -0300

    subdino a versão semi final

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
     let isButtonDisabled = false;
+    let buzina;
 
     document.querySelectorAll('.interactive-image').forEach(imageContainer => {
         imageContainer.addEventListener('click', function () {
             if (!isButtonDisabled) {
                 isButtonDisabled = true;
-
-                // Obtenha os elementos filho (imagem e parágrafo)
                 const image = imageContainer.querySelector('img');
                 const nameElement = imageContainer.querySelector('p');
-
-                // Obtenha os atributos de dados
                 const sound = imageContainer.getAttribute('data-sound');
                 const name = imageContainer.getAttribute('data-name');
 
-                // Crie o elemento de áudio para a buzina clicada
-                const buzina = new Audio(sound);
+                buzina = new Audio(sound);
+                const volumeSlider = document.getElementById('volumeSlider');
 
-                // Reproduza o som correspondente à imagem clicada
+                buzina.volume = volumeSlider.value;
                 buzina.play();
-
-                // Exiba o nome da buzina na consola (pode ser removido em produção)
                 console.log(`Buzina Clicada: ${name}`);
 
-                // Desative o botão por 2 segundos
                 setTimeout(function () {
                     isButtonDisabled = false;
-                }, 2000);
+                }, 1000);
             }
         });
+    });
+
+    const volumeSlider = document.getElementById('volumeSlider');
+    volumeSlider.addEventListener('input', function () {
+        if (buzina) {
+            buzina.volume = volumeSlider.value;
+        }
     });
 });
