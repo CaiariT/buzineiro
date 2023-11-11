72464254f02a1e42571665636315d6d6c9a6feb2
Caiari Tolotti <caiari.tolotti@gmail.com>
2023-11-11 10:10:22 -0300

    subindo mais uma lindeza

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Adiciona um ouvinte de evento a todas as divs com a classe .interactive-image
+    let isButtonDisabled = false;
+
     document.querySelectorAll('.interactive-image').forEach(imageContainer => {
         imageContainer.addEventListener('click', function () {
-            // Obtem os elementos filho (imagem e parágrafo)
-            const image = imageContainer.querySelector('img');
-            const nameElement = imageContainer.querySelector('p');
+            if (!isButtonDisabled) {
+                isButtonDisabled = true;
 
-            // Obtém os atributos de dados
-            const sound = imageContainer.getAttribute('data-sound');
-            const name = imageContainer.getAttribute('data-name');
+                // Obtenha os elementos filho (imagem e parágrafo)
+                const image = imageContainer.querySelector('img');
+                const nameElement = imageContainer.querySelector('p');
 
-            // Criação do elemento de áudio para a buzina clicada
-            const buzina = new Audio(sound);
+                // Obtenha os atributos de dados
+                const sound = imageContainer.getAttribute('data-sound');
+                const name = imageContainer.getAttribute('data-name');
 
-            // Reproduz o som correspondente à imagem clicada
-            buzina.play();
+                // Crie o elemento de áudio para a buzina clicada
+                const buzina = new Audio(sound);
 
-            // Exibe o nome da buzina na consola (pode ser removido em produção)
-            console.log(`Buzina Clicada: ${name}`);
+                // Reproduza o som correspondente à imagem clicada
+                buzina.play();
+
+                // Exiba o nome da buzina na consola (pode ser removido em produção)
+                console.log(`Buzina Clicada: ${name}`);
+
+                // Desative o botão por 2 segundos
+                setTimeout(function () {
+                    isButtonDisabled = false;
+                }, 2000);
+            }
         });
     });
 });
